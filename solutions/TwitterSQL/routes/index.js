3265a937c9d64859ceafd331da040ad31d19ddb5
Omri Bernstein <hippiccolo@gmail.com>
2015-03-17 18:09:19 -0400

    twitter sql: mid-review

--- solutions/TwitterSQL/routes/index.js
+++ solutions/TwitterSQL/routes/index.js
@@ -3,11 +3,36 @@ var router = express.Router();
 var tweetBank = require('../tweetBank');
 module.exports = router;
 
+var models = require('../models'),
+    User = models.User,
+    Tweet = models.Tweet;
+
+// // testing out our new toy
+// User
+// .find(1)
+// .complete(function (err, user) {
+//     console.log('users', user);
+//     console.log('JSON stringified', JSON.stringify(user));
+//     console.log('data values', user.dataValues);
+//     console.log('user name', user.name);
+//     console.log('plainified', user.get({plain: true}));
+// });
+
 router.get('/', function (req, res) {
-    var tweets = tweetBank.list();
-    res.render('index', {
-        title: 'Twitter.js',
-        tweets: tweets
+    // var tweets = tweetBank.list();
+
+    // find all tweets
+    // asynchronous!!!
+    Tweet
+    .findAll()
+    .complete(function (err, tweets) {
+        console.log('tweets', tweets.map(function (tweet) {
+            return tweet.get({plain: true});
+        }));
+        res.render('index', {
+            title: 'Twitter.js',
+            tweets: tweets
+        });
     });
 });
 
