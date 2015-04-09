a510fe13e8a5b41631f9eef9a1fd91b53184904d
NastiaS <anastasiasergienko@gmail.com>
2015-04-09 18:28:46 -0400

    Buttons changed

--- solutions/angular-day-4/public/js/controllers/MainController.js
+++ solutions/angular-day-4/public/js/controllers/MainController.js
@@ -1,6 +1,24 @@
 app.controller('MainController', function ($scope, FlashCardsFactory, $rootScope, $state) {
 
-    
+    $scope.answered = false;
+    $scope.answeredCorrectly = null;
+
+    $scope.answerQuestion = function (answer) {
+
+        if ($scope.answered) {
+            return;
+        }
+
+        $scope.answered = true;
+        $scope.answeredCorrectly = answer.correct;
+
+        if (answer.correct) {
+            ScoreFactory.correct++;
+        } else {
+            ScoreFactory.incorrect++;
+        }
+};
+
     $scope.flashCards = [];
     $scope.cardsLoading = false;
     $scope.categories = [
